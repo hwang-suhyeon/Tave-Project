6347e4910c89b914ed483530219f0b1c13a5dcdf
hwang-suhyeon <zyzy9753@gmail.com>
2022-07-02 02:41:44 +0900

    59 lambda 적용 x

--- optimization/run_optimization.py
+++ optimization/run_optimization.py
@@ -95,8 +95,9 @@ def main(args):
                         s_error_59 += ((latent_code_init[:, i, :] - latent[:, i, :]) ** 2).sum()
                     else:
                         s_error += ((latent_code_init[:, i, :] - latent[:, i, :]) ** 2).sum()
-                # 5번째, 9번째 layer에만 lambda 적용
-                s_error_59 *= args.l2_lambda
+                
+                # 5번째, 9번째 layer에만 lambda 적용 x
+                s_error *= args.l2_lambda
                 
                 l2_loss = s_error + s_error_59
             loss = c_loss + l2_loss + args.id_lambda * i_loss 
